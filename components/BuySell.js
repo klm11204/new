4f722fdff21625203844a6f9e5eab9b36b507129
klm11204 <161860367+klm11204@users.noreply.github.com>
2024-03-01 18:36:27 -0500

    Update BuySell.js

--- components/BuySell.js
+++ components/BuySell.js
@@ -18,9 +18,9 @@ export default function BuySell() {
     await ether.buyBYF(input1);
    };
 
-   //const sell = async (event) => { 
-    //await ether.sellBYF(input1);
-  // };
+   const sell = async (event) => { 
+    await ether.sellBYF(input1);
+   };
 
   useEffect(() => {
     (async () => {
@@ -117,7 +117,7 @@ export default function BuySell() {
                           >
                             Buy
                           </button>
-                      {/*<button
+                      <button
                             className="rounded-lg font-bold uppercase px-3 py-3 shadow-lg hover:shadow-xl outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
                             type="button"
                             id="mint"
@@ -125,7 +125,7 @@ export default function BuySell() {
                             onClick={sell}
                           >
                             Sell
-                          </button>*/}
+                          </button>
                         </div>
                       </div>
                       
