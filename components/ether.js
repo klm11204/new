e3b5949051d1b5d0cce6620b13084eaf4d64a1e0
klm11204 <161860367+klm11204@users.noreply.github.com>
2024-03-09 19:34:19 -0500

    Update ether.js

--- components/ether.js
+++ components/ether.js
@@ -149,7 +149,7 @@ export const mintBadge = async () => {
     } catch (err) {
       if (currentAccount !== null) {
         console.log("Error: ", err.error.message);
-        openNotificationWithIcon("error", err.error.message);
+        openNotificationWithIcon("error", "Go to your MetaMask Wallet. Enter byfcoin.net in the MetaMask browser. Connect your wallet by clicking on the connect button.");
       }
     }
   } else {
